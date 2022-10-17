099c5ada04aeaca65735c0f7272a53d971f8dfd0
Laimne <107036125+Laimne@users.noreply.github.com>
2022-10-17 10:43:26 +0300

    change

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Labas rytas');
+import { server } from "./lib/server.js";
+
+const app = {};
+
+app.init = () => {
+    // susikurti pirminius objektus:
+    // - folder
+    // - file
+    // gauti prisijungima prie DB
+    // paleisti pacia serverio logika
+    server.init();
+
+    // reguliarus procesai:
+    // - atsinaujinti 3rd-party info (API, pvz valiutu kursai)
+    // - duomenu archivavimas
+    // - agregavimas (statistika)
+    // - pasalinti nebereikalingus objektus:
+    //   - failai (tokens)
+    //   - DB optimizavimas
+}
+
+app.init();
+
+export { app };
