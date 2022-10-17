f78e86d07746020f9967afc29184a6b4f2194239
Laimne <107036125+Laimne@users.noreply.github.com>
2022-10-17 10:19:30 +0300

    initial

---
+++ Lib/server.js
@@ -0,0 +1,31 @@
+import http from 'node:http';
+
+const server = {};
+
+server.httpServer = http.createServer((req, res) => {
+    console.log('Gavau uzklausa is kliento...');
+    console.log(req);
+
+    res.end(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>Labas rytas, Lietuva!</h1>
+            </body>
+            </html>`);
+});
+
+server.init = () => {
+    const PORT = 3917;
+
+    server.httpServer.listen(PORT, () => {
+        console.log(`Tavo serveris sukasi: http://localhost:${PORT}`);
+    });
+}
+
+export { server }
