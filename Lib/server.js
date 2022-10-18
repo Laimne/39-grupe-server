40360be9806f2118d2662c7bfedd618515e06a8d
Laimne <107036125+Laimne@users.noreply.github.com>
2022-10-18 15:03:05 +0300

    change

--- Lib/server.js
+++ Lib/server.js
@@ -1,23 +1,60 @@
 import http from 'node:http';
+import { utils } from './utils.js';
+
+import { PageHome } from '../pages/PageHome.js';
+import { PageAbout } from '../pages/PageAbout.js';
+import { PageServices } from '../pages/PageServices.js';
+import { Page404 } from '../pages/Page404.js';
 
 const server = {};
 
 server.httpServer = http.createServer((req, res) => {
-    console.log('Gavau uzklausa is kliento...');
-    console.log(req);
+    const baseURL = `http${req.socket.encryption ? 's' : ''}://${req.headers.host}`;
+    const parsedURL = new URL(req.url, baseURL);
+    const httpMethod = req.method;
+    let trimmedPath = parsedURL.pathname
+        .replace(/^\/+|\/+$/g, '')
+        .replace(/\/\/+/g, '/');
 
-    res.end(`<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                <h1>Labas rytas, Lietuva!</h1>
-            </body>
-            </html>`);
+    console.log(trimmedPath);
+
+    const extension = utils.fileExtension(trimmedPath);
+    const textFileExtensions = ['css', 'js'];
+    const binaryFileExtensions = ['ico', 'png'];
+
+    const isTextFile = textFileExtensions.includes(extension);
+    const isBinaryFile = binaryFileExtensions.includes(extension);
+    const isAPI = trimmedPath.indexOf('/api/') === 0;
+    const isPage = !isTextFile && !isBinaryFile && !isAPI;
+
+    if (isTextFile) {
+        return res.end('STAI TAVO TEKSTINIS FAILAS...');
+    }
+    if (isBinaryFile) {
+        return res.end('STAI TAVO BINARINIS FAILAS...');
+    }
+    if (isAPI) {
+        return res.end('API response...');
+    }
+    if (isPage) {
+        let pageClass = Page404;
+
+        const pageList = {
+            '': PageHome,
+            'about': PageAbout,
+            'services': PageServices,
+        };
+
+        if (pageList[trimmedPath]) {
+            pageClass = pageList[trimmedPath];
+        }
+
+        const HTML = (new pageClass()).render();
+
+        return res.end(HTML);
+    }
+
+    return res.end('ERROR...');
 });
 
 server.init = () => {
@@ -29,3 +66,15 @@ server.init = () => {
 }
 
 export { server }
+
+
+
+
+
+const person = {
+    name: 'petras',
+    age: 99,
+    isMarried: true,
+}
+
+person.age;
